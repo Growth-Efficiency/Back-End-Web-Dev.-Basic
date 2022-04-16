23cdc35f202581b68501c82c7d31b930047d9f25
dongHyo <kdhyo98@gmail.com>
2022-04-16 11:31:17 +0900

    feat : spring viewResolver 파악

--- kdhyo/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ kdhyo/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +14,11 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	// 스프링에서 처음 올라갈 때 application.properties 를 보고 자동으로 올려준다.
+/*	@Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}*/
 
 }
